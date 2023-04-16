05c322894bbee877b6ff77d8a6c255015d817807
kareem444 <kareem963221@gmail.com>
2023-04-17 00:14:57 +0200

    add cors to allow client work in this server

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { join } from 'path';
 import * as express from 'express';
 import * as cookieParser from 'cookie-parser';
+var cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cors())
   app.useGlobalPipes(new ValidationPipe());
   app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.use(cookieParser());
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
